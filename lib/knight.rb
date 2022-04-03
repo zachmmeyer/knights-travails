d1aa3af3051be4fcea7581d65cf1fa1b014b2799
Zach Meyer <zachary.michael.meyer@gmail.com>
2022-04-02 21:16:10 -0400

    Implement logic into knight moves

--- lib/knight.rb
+++ lib/knight.rb
@@ -26,7 +26,25 @@ class Knight
   end
 
   def knight_moves(start_location, end_location)
-    return "You made it in zero moves!\n#{start_location}\n#{end_location}\n" if start_location == end_location
+    # return "You made it in zero moves!\n#{start_location}\n#{end_location}\n" if start_location == end_location
 
+    board = Board.new
+    queue = [start_location]
+    path = [start_location]
+    while queue.any?
+      current_location = queue[0]
+      queue.shift
+      next if board.visited?(current_location)
+
+      board.mark_location_visited(current_location)
+      all_moves = generate_moves(current_location)
+      valid_moves = validate_moves(all_moves)
+      if current_location == end_location
+        # Break loop
+        # Print path
+        return "You made it in 0 moves!\n#{start_location}\n#{end_location}\n"
+      end
+      queue += valid_moves
+    end
   end
 end
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -43,32 +43,32 @@ describe Knight do
     it 'returns the path of the knight not moving' do
       knight = Knight.new
       expected = <<~RESPONSE
-        You made it in zero moves!
+        You made it in 0 moves!
         [0, 0]
         [0, 0]
       RESPONSE
       expect(knight.knight_moves([0, 0], [0, 0])).to eql(expected)
     end
 
-    xit 'returns the path of the knight moving once' do
-      knight = Knight.new
-      expected = <<~RESPONSE
-        You made it in one move!
-        [0, 0]
-        [1, 2]
-      RESPONSE
-      expect(knight.knight_moves([0, 0], [1, 2])).to eql(expected)
-    end
+    # xit 'returns the path of the knight moving once' do
+    #   knight = Knight.new
+    #   expected = <<~RESPONSE
+    #     You made it in 1 move!
+    #     [0, 0]
+    #     [1, 2]
+    #   RESPONSE
+    #   expect(knight.knight_moves([0, 0], [1, 2])).to eql(expected)
+    # end
 
-    xit 'returns the path of the knight moving twice' do
-      knight = Knight.new
-      expected = <<~RESPONSE
-        You made it in two moves!
-        [0, 0]
-        [1, 2]
-        [3, 3]
-      RESPONSE
-      expect(knight.knight_moves([0, 0], [3, 3])).to eql(expected)
-    end
+    # xit 'returns the path of the knight moving twice' do
+    #   knight = Knight.new
+    #   expected = <<~RESPONSE
+    #     You made it in 2 moves!
+    #     [0, 0]
+    #     [1, 2]
+    #     [3, 3]
+    #   RESPONSE
+    #   expect(knight.knight_moves([0, 0], [3, 3])).to eql(expected)
+    # end
   end
 end
