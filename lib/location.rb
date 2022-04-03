2498fc18f4d6c09dd583b519cea812cad15d5dfa
Zach Meyer <zachary.michael.meyer@gmail.com>
2022-04-02 20:34:31 -0400

    Fix locations mutating argument

--- lib/location.rb
+++ lib/location.rb
@@ -3,50 +3,66 @@
 # Location module that contains methods for moving the knight
 module Location
   def left_up_up(location)
-    location[0] -= 1
-    location[1] -= 2
-    location
+    x = location[0]
+    y = location[1]
+    x -= 1
+    y -= 2
+    [x, y]
   end
 
   def left_down_down(location)
-    location[0] -= 1
-    location[1] += 2
-    location
+    x = location[0]
+    y = location[1]
+    x -= 1
+    y += 2
+    [x, y]
   end
 
   def right_up_up(location)
-    location[0] += 1
-    location[1] -= 2
-    location
+    x = location[0]
+    y = location[1]
+    x += 1
+    y -= 2
+    [x, y]
   end
 
   def right_down_down(location)
-    location[0] += 1
-    location[1] += 2
-    location
+    x = location[0]
+    y = location[1]
+    x += 1
+    y += 2
+    [x, y]
   end
 
   def left_left_up(location)
-    location[0] -= 2
-    location[1] -= 1
-    location
+    x = location[0]
+    y = location[1]
+    x -= 2
+    y -= 1
+    [x, y]
   end
 
   def left_left_down(location)
-    location[0] -= 2
-    location[1] += 1
-    location
+    x = location[0]
+    y = location[1]
+    x -= 2
+    y += 1
+    [x, y]
   end
 
   def right_right_up(location)
-    location[0] += 2
-    location[1] -= 1
-    location
+    x = location[0]
+    y = location[1]
+    x += 2
+    y -= 1
+    [x, y]
   end
 
   def right_right_down(location)
-    location[0] += 2
-    location[1] += 1
-    location
+    x = location[0]
+    y = location[1]
+    x += 2
+    y += 1
+    [x, y]
   end
 end
--- spec/location_spec.rb
+++ spec/location_spec.rb
@@ -7,52 +7,92 @@ end
 
 describe Location do
   knight = Knight.new
-
+  location = [3, 3]
   describe '#left_up_up' do
     it 'returns the end location of the knight moving one space to the left and two spaces up' do
-      expect(knight.left_up_up([3, 3])).to eql([2, 1])
+      expect(knight.left_up_up(location)).to eql([2, 1])
+    end
+
+    it 'does not modify original location' do
+      knight.left_up_up(location)
+      expect(knight.left_up_up(location)).to eql([2, 1])
     end
   end
 
   describe '#left_down_down' do
     it 'returns the end location of the knight moving one space to the left and two spaces down' do
-      expect(knight.left_down_down([3, 3])).to eql([2, 5])
+      expect(knight.left_down_down(location)).to eql([2, 5])
+    end
+
+    it 'does not modify original location' do
+      knight.left_down_down(location)
+      expect(knight.left_down_down(location)).to eql([2, 5])
     end
   end
 
   describe '#right_up_up' do
     it 'returns the end location of the knight moving one space to the right and two spaces up' do
-      expect(knight.right_up_up([3, 3])).to eql([4, 1])
+      expect(knight.right_up_up(location)).to eql([4, 1])
+    end
+
+    it 'does not modify original location' do
+      knight.right_up_up(location)
+      expect(knight.right_up_up(location)).to eql([4, 1])
     end
   end
 
   describe '#right_down_down' do
     it 'returns the end location of the knight moving two spaces up and one to the right' do
-      expect(knight.right_down_down([3, 3])).to eql([4, 5])
+      expect(knight.right_down_down(location)).to eql([4, 5])
+    end
+
+    it 'does not modify the original location' do
+      knight.right_down_down(location)
+      expect(knight.right_down_down(location)).to eql([4, 5])
     end
   end
 
   describe '#left_left_up' do
     it 'returns the end location of the knight moving one space to the left twice and once up' do
-      expect(knight.left_left_up([3, 3])).to eql([1, 2])
+      expect(knight.left_left_up(location)).to eql([1, 2])
+    end
+
+    it 'does not modify the original location' do
+      knight.left_left_up(location)
+      expect(knight.left_left_up(location)).to eql([1, 2])
     end
   end
 
   describe '#left_left_down' do
     it 'returns the end location of the knight moving one space to the left twice and once down' do
-      expect(knight.left_left_down([3, 3])).to eql([1, 4])
+      expect(knight.left_left_down(location)).to eql([1, 4])
+    end
+
+    it 'does not modify the original location' do
+      knight.left_left_down(location)
+      expect(knight.left_left_down(location)).to eql([1, 4])
     end
   end
 
   describe '#right_right_up' do
     it 'returns the end location of the knight moving one space to the right twice and once up' do
-      expect(knight.right_right_up([3, 3])).to eql([5, 2])
+      expect(knight.right_right_up(location)).to eql([5, 2])
+    end
+
+    it 'does not modify the original location' do
+      knight.right_right_up(location)
+      expect(knight.right_right_up(location)).to eql([5, 2])
     end
   end
 
   describe '#right_right_down' do
     it 'returns the end location of the knight moving one space to the right twice and once down' do
-      expect(knight.right_right_down([3, 3])).to eql([5, 4])
+      expect(knight.right_right_down(location)).to eql([5, 4])
+    end
+
+    it 'does not modify original location' do
+      knight.right_right_down(location)
+      expect(knight.right_right_down(location)).to eql([5, 4])
     end
   end
 end
