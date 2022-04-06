d5763887ebec16cbb3eff9ffe6465e865af1ddb2
Zach Meyer <zachary.michael.meyer@gmail.com>
2022-04-05 21:48:04 -0400

    Add ability to find shortest path

--- lib/knight.rb
+++ lib/knight.rb
@@ -31,12 +31,25 @@ class Knight
     all_moves
   end
 
+  def generate_path(reversed_edge_list, start_location, end_location, path = [])
+    path << end_location
+    while reversed_edge_list.length > 1
+      path << reversed_edge_list[0][0]
+      next_in_path = reversed_edge_list[0][0]
+      reversed_edge_list.shift
+      reversed_edge_list.shift until reversed_edge_list[0].include?(next_in_path)
+    end
+    path << start_location
+    path.reverse
+  end
+
   def knight_moves(start_location, end_location)
-    # return "You made it in zero moves!\n#{start_location}\n#{end_location}\n" if start_location == end_location
+    return "You made it in zero moves!\n#{start_location}\n#{end_location}\n" if start_location == end_location
 
     board = Board.new
     queue = [start_location]
-    path = [start_location]
+    count = 0
+    edge_list = []
     while queue.any?
       current_location = queue[0]
       queue.shift
@@ -45,12 +58,14 @@ class Knight
       board.mark_location_visited(current_location)
       all_moves = generate_moves(current_location)
       valid_moves = validate_moves(all_moves)
-      new_valid_moves = remove_visited_locations(valid_moves, board)
-      if current_location == end_location
-        # Break loop
-        # Print path
-        return "You made it in 0 moves!\n#{start_location}\n#{current_location}\n"
+      edge_list << [current_location] + valid_moves
+      if edge_list[count].include?(end_location)
+        reversed_edge_list = edge_list.reverse
+        path = generate_path(reversed_edge_list, start_location, end_location)
+        p path
+        return 'Found it!'
       end
+      count += 1
       queue += valid_moves
     end
   end
